b7f5829cc89dd1e92b6bd0b3d227c42f53314fab
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-08-31 13:34:02 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+console.log('we');
+/* 
+
+👉Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+*/
+
+
+//utente sceglie pari o dispari 
+
+//utente inserisce un numero da 1 a 5 
+
+//random 1-5 per Pc (funzione)
+
+//userN + Pc e dichiararre se pari o dispari
+
+//dichiarare vincitore 
+
+
+
+
+
+
+
+
+/* 
+
+👉Palidroma
+Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+*/
